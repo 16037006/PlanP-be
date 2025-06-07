d381378b724f939fa081b249abceace15b88c5ba
16037006 <16037006@suwon.ac.kr>
2025-06-08 01:40:33 +0900

    router에 get_todos_by_title (제목으로 검색) get_todos_between_date (특정기간내 검색) get_todos_this_weekly (특정주간내 검색) get_todos_by_description (description내 단어 탐색 조회) remove_todo (일정 삭제) modify_todo (일정 수정) 추가

--- routers/todo.py
+++ routers/todo.py
@@ -1,12 +1,22 @@
 from datetime import date
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Path
+from fastapi import APIRouter, Depends, Path, Query
 from sqlmodel import Session
 
 from database import get_session
 from models.todo import Todo, TodoBase
-from services.todo import create_todo, get_todo, get_todos_by_plan_date
+from services.todo import(
+    create_todo,
+    get_todo,
+    get_todos_by_plan_date,
+    get_todos_by_title,
+    get_todos_between_date,
+    get_todos_this_weekly,
+    get_todos_by_description,
+    remove_todo,
+    modify_todo
+)
 
 router = APIRouter()
 
@@ -38,3 +48,59 @@ async def get_by_plan_date(
 ):
     todos = get_todos_by_plan_date(session, plan_date)
     return todos
+
+
+@router.get("/get-by-title/")
+async def get_by_title(
+        session: Annotated[Session, Depends(get_session)],
+        title: str = Query(...)
+):
+    todos = get_todos_by_title(session, title)
+    return todos
+
+
+@router.get("/get-between-date/")
+async def get_between_date(
+        session: Annotated[Session, Depends(get_session)],
+        start_date: date,
+        end_date: date
+):
+    todos = get_todos_between_date(session, start_date, end_date)
+    return todos
+
+
+@router.get("/get-this-week/")
+async def get_this_week(
+        session: Annotated[Session, Depends(get_session)],
+        today_date: date = Query(default=date.today())
+):
+    todos = get_todos_this_weekly(session, today_date)
+    return todos
+
+
+@router.get("/get-by-description/")
+async def get_by_description(
+        session: Annotated[Session, Depends(get_session)],
+        description: str = Query(...)
+):
+    todos = get_todos_by_description(session, description)
+    return todos
+
+
+@router.delete("/delete/{todo_id}")
+async def delete(
+        session: Annotated[Session, Depends(get_session)],
+        todo_id: int = Path(ge=0)
+):
+    result = remove_todo(session, todo_id)
+    return {"success": result}
+
+
+@router.put("/modify/{todo_id}")
+async def modify(
+        session: Annotated[Session, Depends(get_session)],
+        todo_id: int = Path(ge=0),
+        todo: TodoBase = ...
+):
+    updated = modify_todo(session, todo_id, todo)
+    return updated
