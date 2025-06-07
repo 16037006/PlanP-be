a076389f58066ab73f1127278473ec8d11149a8e
16037006 <16037006@suwon.ac.kr>
2025-06-07 19:21:56 +0900

    데이터베이스 세팅

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine
-from sqlmodel import Session
 
-# 데이터베이스(SQLite) 설정
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+from database import create_db_and_tables
+from routers import todo
 
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
-
-
-def get_session():
-    with Session(engine) as session:
-        yield session
-
-
+create_db_and_tables()
 app = FastAPI()
+
+# 라우터 등록
+app.include_router(todo.router)
